ea3782452f0aaf6b20dec637a3d48dd776e7e978
InCogNiTo124 <msmetko@msmetko.xyz>
2021-05-08 02:12:40 +0200

    Refactor main to accept outside arguments

--- main.py
+++ main.py
@@ -2,23 +2,37 @@ from belabot.engine.belot import Belot
 from belabot.engine.player import AiPlayer, BigBrain, RandomPlayer
 import time
 import torch
+import argparse
 
-big_brain = BigBrain()
-big_brain.model.eval()
-print(list(big_brain.model.children()))
-players = [
-    AiPlayer("0", big_brain),
-    RandomPlayer("1"),
-    AiPlayer("2", big_brain),
-    RandomPlayer("3"),
-]
-belot = Belot(players)
-#belot.play()
-since = time.time()
-for i in range(100):
-    print(i, end='\t')
-    belot.round(i % 4)
-    new_time = time.time()
-    #log.debug(new_time - since)
-    since = new_time
-big_brain.save()
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--from-checkpoint', type=argparse.FileType('rb'), dest='checkpoint')
+    parser.add_argument('--no-save', action='store_false', dest='do_save')
+    parser.add_argument('--epochs', type=int, default=100)
+    return parser.parse_args()
+
+def main(args):
+    big_brain = BigBrain(args.checkpoint)
+    big_brain.model.eval()
+    print(list(big_brain.model.children()))
+    players = [
+        AiPlayer("0", big_brain),
+        RandomPlayer("1"),
+        AiPlayer("2", big_brain),
+        RandomPlayer("3"),
+    ]
+    belot = Belot(players)
+    #belot.play()
+    since = time.time()
+    for i in range(args.epochs):
+        print(i, end='\t')
+        belot.round(i % 4)
+        new_time = time.time()
+        #log.debug(new_time - since)
+        since = new_time
+    if args.do_save:
+        big_brain.save()
+
+if __name__ == '__main__':
+    args = get_args()
+    main(args)
